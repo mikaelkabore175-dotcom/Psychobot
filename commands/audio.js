8876e35082b9f160797393e4123d3c46af806dbc
ONANA GREGOIRE LEGRAND <gregoirelegrandpsycho@gmail.com>
2025-09-02 01:12:43 +0100

    Update audio.js

--- commands/audio.js
+++ commands/audio.js
@@ -3,9 +3,9 @@ module.exports = {
     run: async ({ sock, msg }) => {
         const from = msg.key.remoteJid;
         await sock.sendMessage(from, {
-            audio: { url: "./fichier.mp3" },
-            mimetype: "audio/mp4",
-            ptt: true
+            audio: { url: "./fichier.mp3" }, // ton fichier
+            mimetype: "audio/mpeg",         // correct pour MP3
+            ptt: true                        // envoyé comme note vocale
         });
     }
 };
